ca7b1c29dcd730ed1a1a630f93d6005f73e85a8d
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2021-12-29 13:04:02 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './animations/navbar';
+import Contact from './Pages/contact';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Developers from './Pages/developers';
+import Home from './components/Home';
+import Team from './Pages/team';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      {
+      //<Developers></Developers>
+      }
+      {
+      //<Contact></Contact>
+      }
+      {
+        <Home></Home>
+      }
+      {
+       //<Team></Team>
+      }
+      </Router>
     </div>
   );
 }
