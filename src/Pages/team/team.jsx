b692e2c7338e09c1d4f217111af794122f9bba36
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2022-01-10 23:30:52 +0530

    team animation

--- src/Pages/team/team.jsx
+++ src/Pages/team/team.jsx
@@ -2,87 +2,12 @@ import React, { useState, useEffect } from "react";
 import anshika from "../../images/anshika.jpeg";
 import "./team.css";
 import axios from "axios";
+import TeamAnimation from "../../components/teamAnimation";
 export default function Team() {
-  const [data, setData] = useState();
-  const [team, setTeam] = useState([]);
-  const [team1, setTeam1] = useState([]);
-  const [team2, setTeam2] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await axios.get(
-          " https://techspardhabackend.herokuapp.com/contacts"
-        );
-        setData(res.data.data.contacts);
-        //console.log(res.data.data.contacts);
-        let array = [];
-        const arr = res.data.data.contacts;
-        for (var i = 0; i < arr.length; i++) {
-          //console.log(arr[i].people[0]);
-          for (var j = 0; j < arr[i].people.length; j++) {
-            console.log(arr[i].people[j]);
-            array.push(arr[i].people[j]);
-          }
-        }
-        let array1 = array;
-        let array2 = array;
-        await setTeam(array);
-        array1 = await array1.sort(() => Math.random() - 0.5);
-        await setTeam1(array1);
-        array2 = await array2.sort(() => Math.random() - 0.5);
-        await setTeam2(array2);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getData();
-  }, []);
   return (
     <div className="team">
-      <div className="team-heading">OUR TEAM</div>
-      <div className="team-slider">
-        <div className="diamond-shape-outer">
-          <div className="team-divide">
-            {team
-              ? team.map((x) => {
-                  return (
-                    <div className="diamond">
-                      <div className="diamond-shape">
-                        <img src={x.imageUrl}></img>
-                      </div>
-                    </div>
-                  );
-                })
-              : "no found"}
-          </div>
-          <div className="team-divide team-middle">
-            {team1
-              ? team1.map((x) => {
-                  return (
-                    <div className="diamond">
-                      <div className="diamond-shape">
-                        <img src={x.imageUrl}></img>
-                      </div>
-                    </div>
-                  );
-                })
-              : "no found"}
-          </div>
-          <div className="team-divide team-bottom">
-            {team2
-              ? team2.map((x) => {
-                  return (
-                    <div className="diamond">
-                      <div className="diamond-shape">
-                        <img src={x.imageUrl}></img>
-                      </div>
-                    </div>
-                  );
-                })
-              : "no found"}
-          </div>
-        </div>
-      </div>
+      <div className="team-heading">TEAM ALTIUS</div>
+      <TeamAnimation />
     </div>
   );
 }
---
+++ src/components/teamAnimation.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./teamAnimation.css";
+export default function TeamAnimation() {
+  const [team, setTeam] = useState([]);
+  const [team1, setTeam1] = useState([]);
+  const [team2, setTeam2] = useState([]);
+  const shuffleArray = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        " https://techspardhabackend.herokuapp.com/contacts"
+      );
+      let array = [];
+      let array1 = [];
+      let array2 = [];
+      const arr = res.data.data.contacts;
+      for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].people.length; j++) {
+          array.push(arr[i].people[j]);
+          array1.push(arr[i].people[j]);
+          array2.push(arr[i].people[j]);
+        }
+      }
+
+      array1 = await shuffleArray(array1);
+      array2 = await shuffleArray(array2);
+
+      setTeam(array);
+      setTeam1(array1);
+      setTeam2(array2);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="team-slider">
+      <div className="diamond-shape-outer">
+        <div className="team-divide">
+          {team
+            ? team.map((x) => {
+                return (
+                  <div className="diamond">
+                    <div className="diamond-shape">
+                      <img src={x.imageUrl}></img>
+                    </div>
+                  </div>
+                );
+              })
+            : "no found"}
+        </div>
+        <div className="team-divide team-middle">
+          {team1
+            ? team1.map((x) => {
+                return (
+                  <div className="diamond">
+                    <div className="diamond-shape">
+                      <img src={x.imageUrl}></img>
+                    </div>
+                  </div>
+                );
+              })
+            : "no found"}
+        </div>
+        <div className="team-divide team-bottom">
+          {team2
+            ? team2.map((x) => {
+                return (
+                  <div className="diamond">
+                    <div className="diamond-shape">
+                      <img src={x.imageUrl}></img>
+                    </div>
+                  </div>
+                );
+              })
+            : "no found"}
+        </div>
+      </div>
+    </div>
+  );
+}
