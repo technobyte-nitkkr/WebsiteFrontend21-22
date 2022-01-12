9df56a1dc827dbdc64b5b2a7d5eec741b6626254
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2022-01-12 15:16:21 +0530

    customise scroller

--- src/Pages/team/team.jsx
+++ src/Pages/team/team.jsx
@@ -8,6 +8,10 @@ export default function Team() {
   return (
     <div className="team">
       <div className="team-heading">TEAM ALTIUS</div>
+      <div className="sub-team-heading">
+        Meet the People who worked countless hours behind the scenes to bring
+        you the Spectacle, that is Altius, the tech-fest of NIT kurukshetra.
+      </div>
       <TeamAnimation />
       <TeamDetails />
     </div>
