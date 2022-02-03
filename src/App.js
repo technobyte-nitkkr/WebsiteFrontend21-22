798f06aba22b1de0119acb5a15d15f9492cf7306
Rahul Rathore <rathorerahul355@gmail.com>
2022-02-04 01:00:47 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className='top-circle'>
+        <div className='top-circle2'></div>
+      </div>
+      <div className='bottom-circle'>
+      <div className='bottom-circle2'></div>
+      </div>
+      <div className='nav'> 
+        <div className='nav-comp'>Home</div>
+        <div className='nav-comp'>About Us</div>
+        <div className='nav-comp'>Events</div>
+        <div className='nav-comp'>Register</div>
+      </div>
+      <div className='head-main'>TECHSPARDHA</div>
+      <div className='time'>24th - 28th February</div>
+      <div className='desc'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</div>
+      <div className='regs'>Register</div>
+      <div className='learn'>Learn More</div>
     </div>
   );
 }
