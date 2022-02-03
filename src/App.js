7e3bc0a0644fee6a043352e6966d38aa2c3c4ad3
TheAlpha27 <utsav.soni.27@gmail.com>
2022-02-04 02:38:18 +0530

    Added circle animations

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ import gear3 from "./assets/gear3.png";
 
 function App() {
   return (
-    <div className="main">
-      <img src={gear1} className="gear1" alt="" />
-      <img src={gear2} className="gear2" alt="" />
-      <img src={gear3} className="gear3" alt="" />
-      <div className='top-circle'>
-        <div className='top-circle2'></div>
+      <div className="main">
+        <img src={gear1} className="gear1" alt="" />
+        <img src={gear2} className="gear2" alt="" />
+        <img src={gear3} className="gear3" alt="" />
+        <div className='top-circle'>
+        </div>
+        <div className='bottom-circle'>
       </div>
-      <div className='bottom-circle'>
-      <div className='bottom-circle2'></div>
-      </div>
-      <div className='nav'> 
+      <div className='nav'>
         <div className='nav-comp'>Home</div>
         <div className='nav-comp'>About Us</div>
         <div className='nav-comp'>Events</div>
@@ -26,8 +24,8 @@ function App() {
       <div className='time'>24th - 28th February</div>
       <div className='desc'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</div>
       <div className='buttons'>
-      <div className='regs'>Register</div>
-      <div className='learn'>Learn More</div>
+        <div className='regs'>Register</div>
+        <div className='learn'>Learn More</div>
       </div>
     </div>
   );
