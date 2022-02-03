34b908e53699ffc6074778234dbffe1fe43d93b1
Rahul Rathore <rathorerahul355@gmail.com>
2022-02-04 01:30:53 +0530

    changed css

--- src/App.js
+++ src/App.js
@@ -18,8 +18,10 @@ function App() {
       <div className='head-main'>TECHSPARDHA</div>
       <div className='time'>24th - 28th February</div>
       <div className='desc'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</div>
+      <div className='buttons'>
       <div className='regs'>Register</div>
       <div className='learn'>Learn More</div>
+      </div>
     </div>
   );
 }
