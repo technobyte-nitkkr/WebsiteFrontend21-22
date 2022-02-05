3f90ceb65dca8ae8c10dd8d9434b2225352698c6
TheAlpha27 <utsav.soni.27@gmail.com>
2022-02-06 03:53:37 +0530

    Added basic animation on load

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,9 +3,12 @@ import '../main/Main.css'
 import gear1 from "../assets/gear1.png";
 import gear2 from "../assets/gear2.png";
 import gear3 from "../assets/gear3.png";
+import { useEffect } from 'react/cjs/react.production.min';
 function Main() {
+  
+
   return (
-    <div className='header-main'>
+    <div id='header-main' className='header-main'>
       <img src={gear1} className="gear1" alt="" />
       <img src={gear2} className="gear2" alt="" />
       <img src={gear3} className="gear3" alt="" />
