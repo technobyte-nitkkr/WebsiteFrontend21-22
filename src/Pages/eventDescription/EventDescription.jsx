acdc4f097e69c556754788c2faba7109ec23a128
Harshit <harshitsharmabtp@gmail.com>
2022-04-10 12:47:56 +0530

    devfolio added

--- src/Pages/eventDescription/EventDescription.jsx
+++ src/Pages/eventDescription/EventDescription.jsx
@@ -8,8 +8,9 @@ import Store from "../../Store/Store.js";
 import SignUpModal from '../SignUpModal';
 import Background from '../../components/Background/Background';
 import BouncingDotsLoader from '../../components/loader/Loader';
-
+import { useNavigate } from 'react-router-dom';
 const EventDescription = () => {
+  let navigate = useNavigate();
   const { category, event } = useParams();
 
   const [Event, setEvent] = useState([]);
@@ -26,11 +27,12 @@ const EventDescription = () => {
   const getInfo = async () => {
     try {
       //fetching this event info
+
       const res = await axios.get(
         `https://us-central1-techspardha-87928.cloudfunctions.net/api2/events/description?eventCategory=${category}&eventName=${event}`
       );
       setEvent(res.data.data);
-
+      
       if (isAuth) {
         //fetching user registered events
         var myevents = [];
@@ -49,13 +51,16 @@ const EventDescription = () => {
         }
 
         await myevents.map((e) => {
-          if (e.eventName == event) {
+          if (e?.eventName == event) {
             setIsRegistered(true);
           }
         })
       }
+
+      setLoading(false);
     } catch (err) {
-      console.log(err);
+      setLoading(true)
+      navigate("/error");
     }
   };
 
@@ -106,6 +111,7 @@ const EventDescription = () => {
         localStorage.setItem('eventsUpdated', true);
       }
     } catch (err) {
+      navigate("/error");
       console.log(err);
       alert(err);
     }
@@ -126,101 +132,134 @@ const EventDescription = () => {
   useEffect(async () => {
     setLoading(true);
     await getInfo();
-    setLoading(false);
   }, []);
+
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+}, []);
 
   return (
     !isLoading ?
       <>
         <Background />
-        <div style={{ textAlign: 'center', color: 'white', width: "100%" }}>
-          <div style={{ fontSize: '6vh' }}>
-            {Event.eventName}
-          </div>
+       {
+          Event && <div style={{ textAlign: 'center', color: 'white', width: "100%" }}>
+            <div style={{ fontSize: '6vh' }}>
+              {Event.eventName}
+            </div>
 
-          <div style={{ padding: '20px' }}>
-            <Row>
-              <Col xs sm={12} lg={4} md={4} style={{ paddingBottom: "20px" }}>
-                <Image src={Event.poster} height="250vw" width="320vw"
-                  style={{ borderRadius: '20px', boxShadow: '0px 4px 10px #4890ff2f' }}
-                  onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = "https://res.cloudinary.com/dvhrzmkwd/image/upload/v1643788204/Unknown39825/eesglbsknedo9vvnclkx.jpg";
-                  }}
-                >
-                </Image>
-                <div style={{ paddingTop: '20px' }} >
-                  <h6>
-                    Start Time : {new Date(Event.startTime).toLocaleString("en-US", { year: "numeric", month: "short", day: "2-digit", hour: "numeric", minute: "2-digit" })}
-                  </h6>
-                  <h6>
-                    End Time : {new Date(Event.endTime).toLocaleString("en-US", { year: "numeric", month: "short", day: "2-digit", hour: "numeric", minute: "2-digit" })}
-                  </h6>
-
-                  {/* {"Event Dates Coming Soon"} */}
-                </div>
-
-                {!isAuth ? <>
-
-                  <Button disabled style={{ color: "white", border: '1px  solid white', background: "transparent" }}>
-                    Login to Register
-                  </Button>
-                </> : <>
-
-                  <Button onClick={handleClick}
-                    style={{
-                      color: "whitesmoke",
-                      backgroundColor: (isRegistered) ? "#e84338" : "#3864e8",
-                      fontSize: "20px",
-                      borderColor: "transparent"
-                    }}>
+            <div style={{ padding: '20px' }}>
+              <Row>
+                <Col xs sm={12} lg={4} md={4} style={{ paddingBottom: "20px" }}>
+                  <Image src={Event.poster} height="250vw" width="320vw"
+                    style={{ borderRadius: '20px', boxShadow: '0px 4px 10px #4890ff2f' }}
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.src = "https://i.ibb.co/hcR8BXn/placeholder.jpg";
+                    }}
+                  >
+                  </Image>
+                  <div style={{ paddingTop: '20px' }} >
                     {
-                      isRegistered ? <>Unregister</> : <>Register</>
+                      Event.endTime > new Date("2022-01-01") ? (<>
+                        <h6>
+                          Start Time : {new Date(Event.startTime).toLocaleString("en-US", { year: "numeric", month: "short", day: "2-digit", hour: "numeric", minute: "2-digit" })}
+                        </h6>
+                        <h6>
+                          End Time : {new Date(Event.endTime).toLocaleString("en-US", { year: "numeric", month: "short", day: "2-digit", hour: "numeric", minute: "2-digit" })}
+                        </h6>
+                      </>): 
+                      <h6>
+                        Events dates coming soon
+                      </h6>
                     }
-                  </Button>
-                  <SignUpModal
-                    show={modalShow}
-                    onHide={handleClose}
-                    user={user}
-                  />
-                </>}
 
-              </Col>
-              <Col >
-                <div style={{ textAlign: 'left', color: 'white', fontSize: '30px' }}>
-                  <h4>Description: </h4>
-                  <p style={{ fontSize: '15px' }}>{Event.description}</p>
-                  {(!Event.rules) ? <>
-                  </> :
-                    <>
-                      <h4>Rules: </h4>
-                      <div style={{ fontSize: '15px' }}>
-                        <ul>
-                        {Event.rules.map((rule) => (
-                          <li style={{margin: "2px"}}>{rule}</li>
-                        ))}
-                        </ul>
-                      </div>
-                    </>}
+                  </div>
 
-                  <h4>Venue: </h4>
-                  <p style={{ fontSize: '15px' }}>{Event.venue}</p>
-                  {(!Event.coordinators) ? <>
-                  </> :
-                    <>
-                      <h4>Coordinators: </h4>
-                      {
-                        Event.coordinators.map((coordinator) => (
-                          <p style={{ fontSize: '15px' }}>{coordinator.coordinator_name} - {coordinator.coordinator_number}</p>
-                        ))}
-                    </>
-                  }
+              {
+                Event.eventName == "Hackshetra" ? <>
+                <div 
+                  class="apply-button" 
+                  data-hackathon-slug="hackshetra" 
+                  data-button-theme="light"
+                  style="height: 44px; width: 312px"
+                ></div>
+                {/* <Button>
+                Apply with devfolio
+                </Button> */}
+                </>:
+                <>
+                  {!isAuth ? <>
 
-                </div>
-              </Col>
-            </Row>
+<Button disabled style={{ color: "white", border: '1px  solid white', background: "transparent" }}>
+  Login to Register
+</Button>
+</> : <>
+
+<Button onClick={handleClick}
+  style={{
+    color: "whitesmoke",
+    backgroundColor: (isRegistered) ? "#e84338" : "#3864e8",
+    fontSize: "20px",
+    borderColor: "transparent"
+  }}>
+  {
+    isRegistered ? <>Unregister</> : <>Register</>
+  }
+</Button>
+<SignUpModal
+  show={modalShow}
+  onHide={handleClose}
+  user={user}
+/>
+</>}
+                </>
+              }
+
+                </Col>
+                <Col >
+                  <div style={{ textAlign: 'left', color: 'white', fontSize: '30px' }}>
+                    <h4>Description: </h4>
+                    <p style={{ fontSize: '15px' }}>{Event.description}</p>
+                    {(!Event.rules) ? <>
+                    </> :
+                      <>
+                        <h4>Rules: </h4>
+                        <div style={{ fontSize: '15px' }}>
+                          <ul>
+                            {Event.rules.map((rule) => (
+                              <li style={{ margin: "2px" }}>{rule}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      </>}
+
+                    <h4>Venue: </h4>
+                    <p style={{ fontSize: '15px' }}>{Event.venue}</p>
+                    {(!Event.coordinators) ? <>
+                    </> :
+                      <>
+                        <h4>Coordinators: </h4>
+                        {
+                          Event.coordinators.map((coordinator) => (
+                            <p style={{ fontSize: '15px' }}>{coordinator.coordinator_name} - {coordinator.coordinator_number}</p>
+                          ))}
+                      </>
+                    }
+
+                  </div>
+                </Col>
+              </Row>
+            </div>
           </div>
-        </div>
+       }
       </> :
       <>
        <BouncingDotsLoader/>
