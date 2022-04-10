12ca291fb79a2cb74e6184fa7273987c99d74e5a
Harshit <harshitsharmabtp@gmail.com>
2022-04-10 12:54:52 +0530

    devfolio button

--- src/Pages/eventDescription/EventDescription.jsx
+++ src/Pages/eventDescription/EventDescription.jsx
@@ -182,8 +182,6 @@ const EventDescription = () => {
                     }
 
                   </div>
-
-<<<<<<< HEAD
               {
                 Event.eventName == "Hackshetra" ? <>
                 <div 
@@ -192,9 +190,6 @@ const EventDescription = () => {
                   data-button-theme="light"
                   style="height: 44px; width: 312px"
                 ></div>
-                {/* <Button>
-                Apply with devfolio
-                </Button> */}
                 </>:
                 <>
                   {!isAuth ? <>
@@ -224,33 +219,6 @@ const EventDescription = () => {
                 </>
               }
 
-=======
-                  {!isAuth ? <>
-
-                    <Button disabled style={{ color: "white", border: '1px  solid white', background: "transparent" }}>
-                      Login to Register
-                    </Button>
-                  </> : <>
-
-                    <Button onClick={handleClick}
-                      style={{
-                        color: "whitesmoke",
-                        backgroundColor: (isRegistered) ? "#e84338" : "#3864e8",
-                        fontSize: "20px",
-                        borderColor: "transparent"
-                      }}>
-                      {
-                        isRegistered ? <>Unregister</> : <>Register</>
-                      }
-                    </Button>
-                    <SignUpModal
-                      show={modalShow}
-                      onHide={handleClose}
-                      user={user}
-                    />
-                  </>}
-
->>>>>>> f257e40fbe6e75d687a5eba7015fdf90bc06fc25
                 </Col>
                 <Col >
                   <div style={{ textAlign: 'left', color: 'white', fontSize: '30px' }}>
